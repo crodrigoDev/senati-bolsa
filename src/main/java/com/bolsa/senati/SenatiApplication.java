29350fdc519474a8d0295f3a095a66edf57d73d8
crodrigoDev <212727257+crodrigoDev@users.noreply.github.com>
2026-05-14 17:21:17 -0500

    backend

---
+++ src/main/java/com/bolsa/senati/SenatiApplication.java
@@ -0,0 +1,13 @@
+package com.bolsa.senati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolsa/senati/SenatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsa.senati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
