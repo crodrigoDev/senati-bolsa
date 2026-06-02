93f178fe1129e80fe28e30a4a22d4653affe9d52
BytheasPls <leovretrl@gmail.com>
2026-06-02 06:36:00 -0500

    Realizar merge con la rama Empresa/backend

--- backend/src/main/java/com/example/senati_bolsa/Models/Usuario.java
+++ backend/src/main/java/com/example/senati_bolsa/Models/Usuario.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.senati_bolsa.Models;
 
 import jakarta.persistence.*;
@@ -21,22 +20,12 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @Table(name = "usuarios")
 public class Usuario extends EntidadBase implements UserDetails{
-=======
-package com.example.senati_bolsa.model;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "usuarios")
-public class Usuario {
->>>>>>> Perfil/Backend
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombres;
     private String apellidos;
     private String numero;
-<<<<<<< HEAD
     @Column(unique = true)
     private String email;
     private String password;
@@ -78,86 +67,3 @@ public class Usuario {
     }
     
 }
-
-
-
-
-
-
-
-
-
-=======
-    private String email;
-    private String password;
-    @Lob
-    @Column(name = "foto_url", columnDefinition = "MEDIUMTEXT")
-    private String fotoUrl;
-    @Column(name = "rol_id")
-    private Integer rolId;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFotoUrl() {
-        return fotoUrl;
-    }
-
-    public void setFotoUrl(String fotoUrl) {
-        this.fotoUrl = fotoUrl;
-    }
-
-    public Integer getRolId() {
-        return rolId;
-    }
-
-    public void setRolId(Integer rolId) {
-        this.rolId = rolId;
-    }
-}
->>>>>>> Perfil/Backend
